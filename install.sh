5c539c17b363cf01f366324237a7431f822fa745
Simon Babay <simon@babay.fr>
2019-07-06 19:47:38 +0200

    Fix install

--- install.sh
+++ install.sh
@@ -3,11 +3,20 @@
 # Install dependencies & neovim
 sudo add-apt-repository -y ppa:neovim-ppa/stable
 sudo apt-get update
-sudo apt-get install -y python-dev python-pip python3-dev python3-pip
-sudo apt-get install -y neovim
+sudo apt-get install -y \
+  python-dev \
+  python-pip \
+  python3-dev \
+  python3-pip \
+  neovim \
+  python-setuptools \
+  python3-setuptools
+
+# Typescript
+yarn global add neovim typescript
 
 # Vim & Python
-pip3 install neovim
+pip3 install neovim pyvim
 
 # FZF install
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
